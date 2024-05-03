7d3b838ef75def80f46c5964ec71abe1a540fee5
Fuji-0910 <157364518+Fuji-0910@users.noreply.github.com>
2024-05-03 17:35:39 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>課題005</title>
+</head>
+
+<body>
+  <P>
+    <?php
+    $score_one = 80;
+    $score_two = 60;
+    $score_three = 55;
+    $score_four = 40;
+    $score_five = 100;
+    $score_six = 25;
+    $score_seven = 80;
+    $score_eight = 95;
+    $score_nine = 30;
+    $score_ten = 60;
+
+    echo ($score_one + $score_two + $score_three + $score_four + $score_five +
+        $score_six + $score_seven + $score_eight + $score_nine + $score_ten) / 10;
+    ?>
+</body>
